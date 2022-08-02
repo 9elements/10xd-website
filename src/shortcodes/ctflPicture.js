a80e5874ce1bf58dec1dbdf8b7f988e066d76810
Nils Binder <nils.binder@icloud.com>
2022-08-02 18:40:06 +0200

    improve shortcode

--- src/shortcodes/ctflPicture.js
+++ src/shortcodes/ctflPicture.js
@@ -7,14 +7,17 @@ Required properties:
 
 Not required, but recommended:
   - alt -> alt for the image (works with ""). Defaults to the image's title defined in Contentful
-  - imgWidth -> The width of the image requested from contentful. Defaults to 600
+  - imgWidth -> The width of the image requested from contentful. Defaults to 800
   - imgHeight -> The height of the image requested from contentful. Defaults to 600
+  !! if you define either imgWidth or imgHeight, the other one will be calculated based on the image's
+  dimensions taken from contentful. If you specify both, the image will be resized. !!
 
 Optional properties:
   - formats -> The image formats generated for the picture. Defaults to ["avif", "webp", "jpg"]
   - widths -> all the widths for the picture elements. Works with [none]. Defaults to [300, 600]
   - sizes -> defines the sizes for the picture. Defaults to "(min-width: 22em) 30vw, 100vw"
   - classes -> add some classes
+  - fit -> if you reisze the image this defines how it should be resized. Defaults to "fill"
 
 Basic usage:
 {% ctflPicture imgObj = myImage, alt="ctfl image", imgWidth="800", imgHeight="600" %}
@@ -34,18 +37,39 @@ Thumbnail example:
 
 const eleventyImage = require("@11ty/eleventy-img");
 
-async function ctflPictureShortcode(serverlessImage) {
-  let imgObj = serverlessImage.imgObj;
+async function ctflPictureShortcode(ctflImage) {
+  let imgObj = ctflImage.imgObj;
   let imgUrl = imgObj.fields.file.url;
   let imgId = imgObj.sys.id;
-  let alt = serverlessImage.alt || imgObj.fields.title;
-  let imgWidth = serverlessImage.imgWidth || 600;
-  let imgHeight = serverlessImage.imgHeight || 600;
-  let formats = serverlessImage.formats;
-  let widths = serverlessImage.widths || [300, 600];
-  let sizes = serverlessImage.sizes;
-  let classes = serverlessImage.classes;
-  let fit = serverlessImage.fit ? serverlessImage.fit : "fill";
+  let alt = ctflImage.alt || imgObj.fields.title;
+  let formats = ctflImage.formats;
+  let widths = ctflImage.widths || [300, 600];
+  let sizes = ctflImage.sizes;
+  let classes = ctflImage.classes;
+  let fit = ctflImage.fit ? ctflImage.fit : "fill";
+
+  let imgWidth = 800;
+  let imgHeight = 600;
+
+  if (ctflImage.imgWidth && ctflImage.imgHeight) {
+    imgWidth = ctflImage.imgWidth;
+    imgHeight = ctflImage.imgHeight;
+  }
+
+  let originSizes = imgObj.fields.file.details.image;
+  let ratio = originSizes.width / originSizes.height;
+
+  if (ctflImage.imgWidth && !ctflImage.imgHeight) {
+    let calculatedHeight = Math.floor(ctflImage.imgWidth / ratio);
+    imgWidth = ctflImage.imgWidth;
+    imgHeight = calculatedHeight;
+  }
+
+  if (!ctflImage.imgWidth && ctflImage.imgHeight) {
+    let calculatedWidth = Math.floor(ctflImage.imgHeight * ratio);
+    imgHeight = ctflImage.imgHeight;
+    imgWidth = calculatedWidth;
+  }
 
   if (imgUrl.startsWith("//")) {
     imgUrl = "https:" + imgUrl;
